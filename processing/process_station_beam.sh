87db42f13e3545eb97e7ac90298ac4aa979504bb
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-02-24 12:29:21 +0800

    added

--- processing/process_station_beam.sh
+++ processing/process_station_beam.sh
@@ -6,7 +6,7 @@ if [[ -n "$1" && "$1" != "-" ]]; then
 fi
 channel=4
 
-echo "python ./hdf5fits_station_beam.py ${station_file} $channel > out 2>&1"
-python ./hdf5fits_station_beam.py ${station_file} $channel > out 2>&1
+echo "python ~/github/station_beam/processing/hdf5fits_station_beam.py ${station_file} $channel > out 2>&1"
+python ~/github/station_beam/processing/hdf5fits_station_beam.py ${station_file} $channel > out 2>&1
 
-# root -l "plot_vs_time.C(\"power_vs_time_ch${channel}.txt\",-1e6,1e6,NULL,0,1000)"
+root -l "plot_vs_time.C(\"power_vs_time_ch${channel}.txt\",-1e6,1e6,NULL,0,1000)"
