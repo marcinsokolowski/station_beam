dadb27f828f659457a5a0ff141f5584346ad57c0
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-03-19 10:29:16 +0800

    bugfix

--- scripts/aavs2/start_aavs2_bighorns.sh
+++ scripts/aavs2/start_aavs2_bighorns.sh
@@ -12,14 +12,14 @@ interval=10800 # 3 hours
 
 za_start=0 # 30
 za_end=25
-nohup calc_eda_sensitvity_all.sh $start_uxtime $interval - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" - - 1800 - - - ${za_start} ${za_end} > za0-25deg.out 2>&1 &
+nohup calc_eda_sensitvity_all.sh $start_time $interval - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" - - 1800 - - - ${za_start} ${za_end} > za0-25deg.out 2>&1 &
 
 sleep 10
 za_start=30 # 30
 za_end=55
-nohup calc_eda_sensitvity_all.sh $start_uxtime $interval - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" - - 1800 - - - ${za_start} ${za_end} > za30-55deg.out 2>&1 &
+nohup calc_eda_sensitvity_all.sh $start_time $interval - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" - - 1800 - - - ${za_start} ${za_end} > za30-55deg.out 2>&1 &
 
 sleep 10
 za_start=60 # 30
 za_end=90
-nohup calc_eda_sensitvity_all.sh $start_uxtime $interval - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" - - 1800 - - - ${za_start} ${za_end} > za60-90deg.out 2>&1 &
+nohup calc_eda_sensitvity_all.sh $start_time $interval - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" - - 1800 - - - ${za_start} ${za_end} > za60-90deg.out 2>&1 &
