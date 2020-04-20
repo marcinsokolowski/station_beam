9570513f3b30de7cd6957b0bb0d8fa16e27a748a
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-04-20 17:04:58 +0800

    corr

--- django/skalow_station_sensitivity/skalow_station_sensitivity/views.py
+++ django/skalow_station_sensitivity/skalow_station_sensitivity/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. Starting ska sensitivity database application")
     
