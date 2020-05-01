8e837cb1e6bbb11609c26e18680c9566b3f4ad0c
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-01 13:43:59 +0800

    added

---
+++ scripts/aavs2/start_aavs2_sensitivity_simul.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+channel=204
+if [[ -n "$1" && "$1" != "-" ]]; then
+   channel=$1
+fi
+
+
+path=`pwd`
+dir_path=`dirname $path`
+utc=`basename $dir_path`
+
+ux_start=`date2date -ut2ux=$utc | awk '{print $3;}'`
+mwa_coarse_channel=`echo $channel | awk '{mwa_cc=$1*((400.00/512.00)/1.28);printf("%d\n",mwa_cc);}'`
+
+export PATH=~/github/station_beam/:/opt/anaconda2/bin:$PATH
+
+# echo "nohup calc_eda_sensitvity_all.sh $ux_start 86400 - \"--use_beam_fits --station_name=EDA --size=512 --trcv_type=trcv_eda2\" $mwa_coarse_channel - 300 400 400 > out 2>&1 &"
+# nohup calc_eda_sensitvity_all.sh $ux_start 86400 - "--use_beam_fits --station_name=EDA --size=512 --trcv_type=trcv_eda2" $mwa_coarse_channel - 300 400 400 > out 2>&1 &
+
+echo "cp ~/aavs-calibration/config/aavs2/antenna_locations.txt antenna_locations_aavs2.txt"
+cp ~/aavs-calibration/config/aavs2/antenna_locations.txt antenna_locations_aavs2.txt
+
+echo "nohup calc_eda_sensitvity_all.sh $ux_start 86400 - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" $mwa_coarse_channel - 300 400 400 > out 2>&1 &"
+nohup calc_eda_sensitvity_all.sh $ux_start 86400 - "--use_beam_fits --station_name=SKALA4 --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_aavs2.txt" $mwa_coarse_channel - 300 400 400 > out 2>&1 &
+
+# ~/github/station_beam/tools/merge_sensitivity_loop.sh
