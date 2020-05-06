413a4f2aa38c84156c37422336401bb99369144d
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-06 20:51:23 +0800

    first step

--- django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
+++ django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
     
+    
+class ParametersForm(forms.Form):
+    frequency_mhz = forms.FloatField(label='Frequency [MHz]')
+    
+    
--- django/skalow_station_sensitivity/sensitivity_vs_lst/views.py
+++ django/skalow_station_sensitivity/sensitivity_vs_lst/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
-from .forms import NameForm
+from .forms import ParametersForm
 
 def get_name(request):
     # if this is a POST request we need to process the form data
@@ -20,6 +20,24 @@ def get_name(request):
         form = NameForm()
 
     return render(request, 'name.html', {'form': form})
+
+def get_parameters(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ParametersForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ParametersForm()
+
+    return render(request, 'parameters.html', {'form': form})
 
 # Create your views here.
 def blog_list(request):
