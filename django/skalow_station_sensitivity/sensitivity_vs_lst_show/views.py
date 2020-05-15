912a8e599cfd797ac9380180f098ae1a64d7b025
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-15 19:01:22 +0800

    first tries

--- django/skalow_station_sensitivity/sensitivity_vs_lst_show/views.py
+++ django/skalow_station_sensitivity/sensitivity_vs_lst_show/views.py
@@ -27,9 +27,9 @@ def sensitivity_vs_lst_show(request):
    elevation_deg = float( params['elevation_deg'] )
    za_deg = (90.00 - elevation_deg)
    station = "EDA2"
-   db_path = ( "%s/ska_station_sensitivity_%s.db" % (config.sensitivity_db_path,station)
+   db_path = ( "%s/" % (config.sensitivity_db_path) )
    
-   print("Parameters = %s -> %.4f MHz, (az,el) = (%.4f,%.4f) [deg]" % (params,frequency_mhz,azimuth_deg,elevation_deg))
+   print("Parameters = %s -> %.4f MHz, (az,el) = (%.4f,%.4f) [deg] , station = %s, db_path = %s" % (params,frequency_mhz,azimuth_deg,elevation_deg,station,db_path))
    
    (lst_x,aot_x,sefd_x, lst_y,aot_y,sefd_y) = sensitivity_db.get_sensitivity_lstrange( azimuth_deg, za_deg, frequency_mhz, lst_start=0, lst_end=24, time_step=300, station="EDA2" , db_path=db_path )
 
