2ea8cca88f56c24f73bf1115fc333e3b4f252508
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-15 18:22:59 +0800

    azimuth and elevation added

--- django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
+++ django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
@@ -6,5 +6,7 @@ class NameForm(forms.Form):
     
 class ParametersForm(forms.Form):
     frequency_mhz = forms.FloatField(label='Frequency [MHz]',min_value=0.0, max_value=350.0) # ,max_digits=5,decimal_places=1)
+    azimuth_deg   = forms.FloatField(label='Azimuth [deg]',min_value=0.0, max_value=360.0) # ,max_digits=5,decimal_places=1)
+    elevation_deg = forms.FloatField(label='Elevation [deg]',min_value=0.0, max_value=90.0) # ,max_digits=5,decimal_places=1)
     
     
