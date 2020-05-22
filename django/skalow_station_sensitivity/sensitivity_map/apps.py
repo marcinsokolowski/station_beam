0ec0dafba1fe65611b7ddd7a8be0eebfe04999e2
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-22 17:25:46 +0800

    added

---
+++ django/skalow_station_sensitivity/sensitivity_map/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SensitivityMapConfig(AppConfig):
+    name = 'sensitivity_map'
---
+++ django/skalow_station_sensitivity/sensitivity_map_show/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SensitivityMapShowConfig(AppConfig):
+    name = 'sensitivity_map_show'
