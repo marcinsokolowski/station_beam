7e8d8d7be7ca8cfcbe130dc1a1b2c5ac1715b227
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-22 16:26:06 +0800

    not yet working

--- django/skalow_station_sensitivity/sensitivity_vs_freq_show/views.py
+++ django/skalow_station_sensitivity/sensitivity_vs_freq_show/views.py
@@ -38,7 +38,7 @@ def mkdir_p(path):
 
 
 # Create your views here.
-def sensitivity_vs_lst_show(request):
+def sensitivity_vs_freq_show(request):
    zipfile = True
    post = Post.objects.all()
 
@@ -51,7 +51,7 @@ def sensitivity_vs_lst_show(request):
       print("POST !!!")
       params = request.POST
 
-   frequency_mhz = float( params['frequency_mhz'] )
+   lst_hours = float( params['lst_hours'] )
    azimuth_deg   = float( params['azimuth_deg'] )
    elevation_deg = float( params['elevation_deg'] )
    mode          = int( params['mode'] )
@@ -64,34 +64,34 @@ def sensitivity_vs_lst_show(request):
    print("DEBUG : station = %s" % (station))
    db_path = ( "%s/" % (config.sensitivity_db_path) )
    
-   print("Parameters = %s -> %.4f MHz, (az,el) = (%.4f,%.4f) [deg] , station = %s, db_path = %s , mode = %d ( return_zip_file = %s)" % (params,frequency_mhz,azimuth_deg,elevation_deg,station,db_path,mode,return_zip_file))
+   print("Parameters = %s -> LST = %.4f [hours], (az,el) = (%.4f,%.4f) [deg] , station = %s, db_path = %s , mode = %d ( return_zip_file = %s)" % (params,lst_hours,azimuth_deg,elevation_deg,station,db_path,mode,return_zip_file))
    
-   (lst_x,aot_x,sefd_x, lst_y,aot_y,sefd_y) = sensitivity_db.get_sensitivity_lstrange( azimuth_deg, za_deg, frequency_mhz, lst_start=0, lst_end=24, time_step=300, station=station , db_path=db_path )
+# def get_sensitivity_azzalst( az_deg , za_deg , lst_hours , 
+#                             station="EDA2", db_base_name="ska_station_sensitivity", db_path="sql/", 
+#                             db_lst_resolution=0.5, db_ang_res_deg=5.00 ) :
+#    return ( numpy.array(out_freq_x), numpy.array(out_aot_x) , numpy.array(out_sefd_x),
+#             numpy.array(out_freq_y), numpy.array(out_aot_y) , numpy.array(out_sefd_y) )   
+   ( freq_x,aot_x,sefd_x, freq_y,aot_y,sefd_y ) = sensitivity_db.get_sensitivity_azzalst( azimuth_deg, za_deg, lst_hours, station=station, db_path=db_path )
 
+#   return render(request,"sensitivity_vs_freq_show/index.html" )
    
    # 
    save_output_path = config.save_output_path
    mkdir_p( save_output_path )
 
    # create plots :   
-   print("DEBUG : calling sensitivity_db.plot_sensitivity_vs_lst (saving to %s)" % (save_output_path))
-   output_file_base = "%s_sensitivity_lst0-24h_az%.2fdeg_za_%.2fdeg_%.2fMHz" % (station,azimuth_deg,za_deg,frequency_mhz)
+   print("DEBUG : calling sensitivity_db.plot_sensitivity_vs_freq (saving to %s)" % (save_output_path))
+   output_file_base = "%s_sensitivity_az%.2fdeg_za_%.2fdeg_%.2fhours" % (station,azimuth_deg,za_deg,lst_hours)
+   sensitivity_db.plot_sensitivity( freq_x, aot_x, freq_y, aot_y, output_file_base=output_file_base )
+   
+   return render(request,"sensitivity_vs_freq_show/index.html" )
+   
    (png_image_path,buf) = sensitivity_db.plot_sensitivity_vs_lst( lst_x, aot_x, lst_y, aot_y, lst_start=0, lst_end=20, azim_deg=azimuth_deg, za_deg=za_deg, freq_mhz=frequency_mhz, output_file_base=output_file_base, do_show=False, save_output_path=save_output_path )
    
    # def save_sens_vs_lst_file( lst_x, aot_x, sefd_x, lst_y, aot_y, sefd_y out_file_base ) :
    out_file_name = save_output_path + "/" + output_file_base
    (text_file) = sensitivity_db.save_sens_vs_lst_file( lst_x, aot_x, sefd_x, lst_y, aot_y, sefd_y, out_file_name )   
 
-#   print("DEBUG : plotting image %s" % (png_image_path))
-   
-#   template = loader.get_template('sensitivity_vs_lst_show/index.html')
-
-#   return render_to_response('sensitivity_vs_lst_show/index.html', { 'image': png_image_path }, context_instance=RequestContext(request) )
-   
-#   {'form': form,'obs': observation, 
-#                                                           'setting': setting, 'error': error,
-#                                                           'image': image_path, 'obs_link': obs_link}, 
-#                                                           context_instance=RequestContext(request))
 
    buf.seek(0)
    string = base64.b64encode(buf.read())
@@ -144,6 +144,6 @@ def sensitivity_vs_lst_show(request):
    args = { 'image':uri , 'zipfile':zip_file_name }
    print("DEBUG : mode = %d" % (mode))
          
-   return render(request,"sensitivity_vs_lst_show/index.html" , args ) # , context_instance=RequestContext(request) )
+   return render(request,"sensitivity_vs_freq_show/index.html" , args ) # , context_instance=RequestContext(request) )
 #   render(request,"sensitivity_vs_lst_show/index.html" , args ) # , context_instance=RequestContext(request) )
 #   return response
--- django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
+++ django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
@@ -6,9 +6,9 @@ class NameForm(forms.Form):
     
     
 class ParametersForm(forms.Form):
-    frequency_mhz = forms.FloatField( label='Frequency [MHz]' , min_value=0.0 , max_value=350.0) # ,max_digits=5,decimal_places=1)
-    azimuth_deg   = forms.FloatField( label='Azimuth [deg]'   , min_value=0.0 , max_value=360.0) # ,max_digits=5,decimal_places=1)
-    elevation_deg = forms.FloatField( label='Elevation [deg]' , min_value=0.0 , max_value=90.0) # ,max_digits=5,decimal_places=1)
+    frequency_mhz = forms.FloatField( label='Frequency [MHz]' , min_value=40.0 , max_value=350.0, initial=160.00 ) # ,max_digits=5,decimal_places=1)
+    azimuth_deg   = forms.FloatField( label='Azimuth [deg]'   , min_value=0.0 , max_value=360.0, initial=0.00 ) # ,max_digits=5,decimal_places=1)
+    elevation_deg = forms.FloatField( label='Elevation [deg]' , min_value=0.0 , max_value=90.0, initial=90.00 ) # ,max_digits=5,decimal_places=1)
 
     STATION_CHOICES = [
        ('EDA2', 'EDA2'),
