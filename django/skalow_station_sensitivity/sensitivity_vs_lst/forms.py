98e821b300fb752cabd5b42a0b3bd8f4fc77d796
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-22 10:29:10 +0800

    nicer look

--- django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
+++ django/skalow_station_sensitivity/sensitivity_vs_lst/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
@@ -9,6 +10,11 @@ class ParametersForm(forms.Form):
     azimuth_deg   = forms.FloatField(label='Azimuth [deg]',min_value=0.0, max_value=360.0) # ,max_digits=5,decimal_places=1)
     elevation_deg = forms.FloatField(label='Elevation [deg]',min_value=0.0, max_value=90.0) # ,max_digits=5,decimal_places=1)
 
-    CHOICES = [('1', 'Image'), ('2', 'Zip file')]    
-    mode = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES, label='mode')
+    STATION_CHOICES = [
+       ('eda2', 'EDA2'),
+       ('aavs2', 'AAVS2')
+    ] 
+    station_name = forms.CharField(label='Station', widget=forms.Select(choices=STATION_CHOICES))   
     
+    CHOICES = [('1', 'Show image'), ('2', 'Save zip file (data and image)')]    
+    mode = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES, label='mode', initial='1')
