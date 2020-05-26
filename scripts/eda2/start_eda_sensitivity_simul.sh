0739ace8ba754fbb156db198fad41c4834fc66e2
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-26 12:02:37 +0800

    bugfix

--- scripts/eda2/start_eda_sensitivity_simul.sh
+++ scripts/eda2/start_eda_sensitivity_simul.sh
@@ -7,9 +7,10 @@ fi
 
 path=`pwd`
 dir_path=`dirname $path`
-utc=`basename $dir_path`
 
-ux_start=`date2date -ut2ux=$utc | awk '{print $3;}'`
+local_time=`basename $dir_path`
+ux_start=`date2date -local2ux=$local_time | awk '{print $1;}'`
+
 mwa_coarse_channel=`echo $channel | awk '{mwa_cc=$1*((400.00/512.00)/1.28);printf("%d\n",mwa_cc);}'` # this is MWA 1.28-MHz coarse channel ( 107*1.28 = 136.96 MHz )
 
 export PATH=~/github/station_beam/:/opt/anaconda2/bin:$PATH
