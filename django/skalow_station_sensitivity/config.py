cbab5851ede6f39a623801226702d6e62b3e008f
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-27 16:33:45 +0800

    corr

--- django/skalow_station_sensitivity/config.py
+++ django/skalow_station_sensitivity/config.py
@@ -2,9 +2,9 @@ from string import Template
 import os
 
 # configuration :
-station_beam_path = "$(HOME)/github/station_beam"
-t = Template( station_beam_path )
-sensitivity_db_path = t.substitute(os.environ)
+station_beam_path_tmp = "$HOME/github/station_beam"
+t = Template( station_beam_path_tmp )
+station_beam_path = t.substitute(os.environ)
 
 
 sensitivity_db_path = station_beam_path + "/sql/" 
