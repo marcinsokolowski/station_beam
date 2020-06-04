6aa13e84ad016970cf78dae2411aa06124221afd
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-06-04 11:04:07 +0800

    rounding of MWA channel corrected

--- scripts/aavs2/start_aavs2_sensitivity_simul.sh
+++ scripts/aavs2/start_aavs2_sensitivity_simul.sh
@@ -16,7 +16,36 @@ dir_path=`dirname $path`
 local_time=`basename $dir_path`
 ux_start=`date2date -local2ux=$local_time | awk '{print $1;}'`
 
-mwa_coarse_channel=`echo $channel | awk '{mwa_cc=$1*((400.00/512.00)/1.28);printf("%d\n",mwa_cc);}'`
+# mwa_coarse_channel=`echo $channel | awk '{mwa_cc=$1*((400.00/512.00)/1.28);printf("%d\n",mwa_cc);}'` # this is MWA 1.28-MHz coarse channel ( 107*1.28 = 136.96 MHz )
+mwa_coarse_channel=`echo $channel | awk '
+function round(x)
+{
+   ival = int(x)    # integer part, int() truncates
+
+   # see if fractional part
+   if (ival == x)   # no fraction
+      return ival   # ensure no decimals
+
+   if (x < 0) {
+      aval = -x     # absolute value
+      ival = int(aval)
+      fraction = aval - ival
+      if (fraction >= .5)
+         return int(x) - 1   # -2.5 --> -3
+      else
+         return int(x)       # -2.3 --> -2
+   } else {
+      fraction = x - ival
+      if (fraction >= .5)
+         return ival + 1
+      else
+         return ival
+   }
+}
+{
+	mwa_cc=$1*((400.00/512.00)/1.28);print round(mwa_cc);
+}'`
+
 
 export PATH=~/github/station_beam/:/opt/anaconda2/bin:$PATH
 
