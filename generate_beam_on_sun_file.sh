ca5d355966bb72de2c285acf5a7dabd442406920
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-08-28 11:12:01 +0800

    station parameter added

--- generate_beam_on_sun_file.sh
+++ generate_beam_on_sun_file.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ls *.hdf5 > hdf5_list
+station_name=EDA2
+if [[ -n "$1" && "$1" != "-" ]]; then
+   station_name=$1
+fi
 
-echo "python ~/github/station_beam/fits_beam.py --infile_hdf5list=hdf5_list --outfile_beam_on_sun=beam_on_sun.txt"
-python ~/github/station_beam/fits_beam.py --infile_hdf5list=hdf5_list --outfile_beam_on_sun=beam_on_sun.txt
+ls -tr *.hdf5 > hdf5_list
+
+echo "python ~/github/station_beam/fits_beam.py --infile_hdf5list=hdf5_list --outfile_beam_on_sun=beam_on_sun.txt --station=${station_name}"
+python ~/github/station_beam/fits_beam.py --infile_hdf5list=hdf5_list --outfile_beam_on_sun=beam_on_sun.txt --station=${station_name}
 
