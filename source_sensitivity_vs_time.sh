62758e126ea39f0584740d674fc9686e6a36a984
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-11-15 19:34:21 +0800

    correction

--- source_sensitivity_vs_time.sh
+++ source_sensitivity_vs_time.sh
@@ -54,12 +54,12 @@ ra=333.607249950 # degree
 dec=-17.02661111 # degree
 object_radec $object
 
-ra=333.607249950
+# Can overwrite RA,DEC with parameters:
 if [[ -n "$4" && "$4" != "-" ]]; then
    ra=$4
 fi
 
-dec=-17.02661111
+# Can overwrite RA,DEC with parameters:
 if [[ -n "$5" && "$5" != "-" ]]; then
    dec=$5
 fi
@@ -95,44 +95,44 @@ fi
 gps=$gps_start
 while [[ $gps -le $gps_end ]];
 do
-   echo "python ~/github/station_beam//eda_sensitivity.py --freq=${freq_mhz} -p None -g ${gps}  -m analytic --ra=${ra} --dec=${dec} --outsens_file=${object}_eda_sensitivity --outfile_mode=a --trcv_type=trcv_from_skymodel_with_err  --nos11 --header=HEADER  --use_beam_fits --station_name=${staton_name} --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_${station}.txt --projection=aee"
-   python ~/github/station_beam//eda_sensitivity.py --freq=${freq_mhz} -p None -g ${gps}  -m analytic --ra=${ra} --dec=${dec} --outsens_file=${object}_eda_sensitivity --outfile_mode=a --trcv_type=trcv_from_skymodel_with_err  --nos11 --header=HEADER  --use_beam_fits --station_name=${staton_name} --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_${station}.txt --projection=aee
+   echo "python ~/github/station_beam//eda_sensitivity.py --freq=${freq_mhz} -p None -g ${gps}  -m analytic --ra=${ra} --dec=${dec} --outsens_file=${object}_${station}_sensitivity --outfile_mode=a --trcv_type=trcv_from_skymodel_with_err  --nos11 --header=HEADER  --use_beam_fits --station_name=${staton_name} --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_${station}.txt --projection=aee"
+   python ~/github/station_beam//eda_sensitivity.py --freq=${freq_mhz} -p None -g ${gps}  -m analytic --ra=${ra} --dec=${dec} --outsens_file=${object}_${station}_sensitivity --outfile_mode=a --trcv_type=trcv_from_skymodel_with_err  --nos11 --header=HEADER  --use_beam_fits --station_name=${staton_name} --size=512 --trcv_type=trcv_aavs2_vs_za_deg --antenna_locations=antenna_locations_${station}.txt --projection=aee
 
    gps=$(($gps+$step))
 done   
 
 # elev vs. azimuth :
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){azim=$10;elev=(90.00-$15);}}else{if(azim>180){azim=azim-360;}if(azim>=-120 && azim<=120){print azim" "elev;}}}' ${object}_eda_sensitivity_XX.txt > ${object}_elev_vs_azimuth.txt
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){azim=$10;elev=(90.00-$15);}}else{if(azim>180){azim=azim-360;}if(azim>=-120 && azim<=120){print azim" "elev;}}}' ${object}_${station}_sensitivity_XX.txt > ${object}_elev_vs_azimuth.txt
 
 # get A/T vs. azimuth 
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$2;}}}' ${object}_eda_sensitivity_XX.txt | sort -n > ${object}_eda_sensitivity_vs_azim_XX.txt
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$2;}}}' ${object}_eda_sensitivity_YY.txt | sort -n > ${object}_eda_sensitivity_vs_azim_YY.txt
-ls ${object}_eda_sensitivity_vs_azim_XX.txt ${object}_eda_sensitivity_vs_azim_YY.txt > ${object}_eda_sensitivity_vs_azim.list
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$2;}}}' ${object}_${station}_sensitivity_XX.txt | sort -n > ${object}_${station}_sensitivity_vs_azim_XX.txt
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$2;}}}' ${object}_${station}_sensitivity_YY.txt | sort -n > ${object}_${station}_sensitivity_vs_azim_YY.txt
+ls ${object}_${station}_sensitivity_vs_azim_XX.txt ${object}_${station}_sensitivity_vs_azim_YY.txt > ${object}_${station}_sensitivity_vs_azim.list
 
 # get A/T vs. elevation :
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=90.00-$15;}}else{if(elev>0){print elev" "$2;}}}' ${object}_eda_sensitivity_XX.txt > ${object}_eda_sensitivity_vs_elev_XX.txt
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=90.00-$15;}}else{if(elev>0){print elev" "$2;}}}' ${object}_eda_sensitivity_YY.txt > ${object}_eda_sensitivity_vs_elev_YY.txt
-ls ${object}_eda_sensitivity_vs_elev_XX.txt ${object}_eda_sensitivity_vs_elev_YY.txt > ${object}_eda_sensitivity_vs_elev.list
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=90.00-$15;}}else{if(elev>0){print elev" "$2;}}}' ${object}_${station}_sensitivity_XX.txt > ${object}_${station}_sensitivity_vs_elev_XX.txt
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=90.00-$15;}}else{if(elev>0){print elev" "$2;}}}' ${object}_${station}_sensitivity_YY.txt > ${object}_${station}_sensitivity_vs_elev_YY.txt
+ls ${object}_${station}_sensitivity_vs_elev_XX.txt ${object}_${station}_sensitivity_vs_elev_YY.txt > ${object}_${station}_sensitivity_vs_elev.list
 
 # get A vs. azimuth 
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$4;}}}' ${object}_eda_sensitivity_XX.txt | sort -n > ${object}_eda_aeff_vs_azim_XX.txt
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$4;}}}' ${object}_eda_sensitivity_YY.txt | sort -n > ${object}_eda_aeff_vs_azim_YY.txt
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$4;}}}' ${object}_${station}_sensitivity_XX.txt | sort -n > ${object}_eda_aeff_vs_azim_XX.txt
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$4;}}}' ${object}_${station}_sensitivity_YY.txt | sort -n > ${object}_eda_aeff_vs_azim_YY.txt
 ls ${object}_eda_aeff_vs_azim_XX.txt ${object}_eda_aeff_vs_azim_YY.txt > ${object}_eda_aeff_vs_azim.list
 
 # get T_sys vs. azimuth 
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$3;}}}' ${object}_eda_sensitivity_XX.txt | sort -n > ${object}_eda_tsys_vs_azim_XX.txt
-awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$3;}}}' ${object}_eda_sensitivity_YY.txt | sort -n > ${object}_eda_tsys_vs_azim_YY.txt
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$3;}}}' ${object}_${station}_sensitivity_XX.txt | sort -n > ${object}_eda_tsys_vs_azim_XX.txt
+awk -v elev=-1 '{if($1=="#"){if($2=="HEADER"){elev=$10;}}else{if(elev>180){elev=elev-360;}if(elev>=-120 && elev<=120){print elev" "$3;}}}' ${object}_${station}_sensitivity_YY.txt | sort -n > ${object}_eda_tsys_vs_azim_YY.txt
 ls ${object}_eda_tsys_vs_azim_XX.txt ${object}_eda_tsys_vs_azim_YY.txt > ${object}_eda_tsys_vs_azim.list
 
 # plots may not work without root package installed :
 mkdir -p images/
-root -b -q -l "plotNfiles_AoT_vs_azim_PAPER_LFAAREQ_TEST.C(\"${object}_eda_sensitivity_vs_azim.list\",${freq_mhz},\"${object}_elev_vs_azimuth.txt\")"
-root -b -q -l "plotNfiles_AoT_vs_elev_PAPER_LFAAREQ_TEST.C(\"${object}_eda_sensitivity_vs_elev.list\",${freq_mhz})"
+root -b -q -l "plotNfiles_AoT_vs_azim_PAPER_LFAAREQ_TEST.C(\"${object}_${station}_sensitivity_vs_azim.list\",${freq_mhz},\"${object}_elev_vs_azimuth.txt\")"
+root -b -q -l "plotNfiles_AoT_vs_elev_PAPER_LFAAREQ_TEST.C(\"${object}_${station}_sensitivity_vs_elev.list\",${freq_mhz})"
 
 root -b -q -l "plotNfiles_Aeff_vs_azim_PAPER_LFAAREQ_TEST.C(\"${object}_eda_aeff_vs_azim.list\",${freq_mhz})"
 root -b -q -l "plotNfiles_Tsys_vs_azim_PAPER_LFAAREQ_TEST.C(\"${object}_eda_tsys_vs_azim.list\",${freq_mhz})"
 
 # checks :
-awk -v cnt=0 -v mean=0.00 -v elev=-1 -v azim=-1 '{if($1=="#"){if($2=="HEADER"){azim=$10;elev=(90.00-$15);}}else{if(azim>180){azim=azim-360;}if(elev>=45){mean+=$2;cnt+=1;print azim" "elev" "$2" "mean/cnt;}}}' ${object}_eda_sensitivity_XX.txt
+awk -v cnt=0 -v mean=0.00 -v elev=-1 -v azim=-1 '{if($1=="#"){if($2=="HEADER"){azim=$10;elev=(90.00-$15);}}else{if(azim>180){azim=azim-360;}if(elev>=45){mean+=$2;cnt+=1;print azim" "elev" "$2" "mean/cnt;}}}' ${object}_${station}_sensitivity_XX.txt
 
 
