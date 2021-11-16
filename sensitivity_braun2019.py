b6f458127545c5acf7cc8253da68d46bf0e68279
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-11-16 21:02:12 +0800

    added sensitivity in Table 9 in Braun et al. (2019)

--- sensitivity_braun2019.py
+++ sensitivity_braun2019.py
@@ -8,11 +8,12 @@ from scipy.interpolate import interp1d
 
 
 
-# Based on : /home/msok/Desktop/EDA/data/sensitivity/20161207_eda_to_mwa/3c444_evening/full_band_sensitivity$ awk '{print $4" "$1;}' lfaa_requirements_20170330.rw_email
-# /home/msok/Desktop/EDA2/papers/2020/M_Sokolowski/EuCAP/M_Sokolowski/logbook/20200929_lfaa_specifications.odt
-def lfaa_ska( freq_mhz, interpolation_kind='cubic' ) :
-   freq_mhz_array = [ 50, 55,  80, 110, 140, 160, 220, 280, 340, 345, 350 ] # /home/msok/Desktop/EDA/data/sensitivity/20161207_eda_to_mwa/3c444_evening/full_band_sensitivity$ awk '{printf("%s, ",$4);}' lfaa_requirements_20170330.rw_email
-   station_aot    = [ 68, 70, 232, 531, 588, 610, 614, 576, 522, 515, 516 ] # /home/msok/Desktop/EDA/data/sensitivity/20161207_eda_to_mwa/3c444_evening/full_band_sensitivity$ awk '{printf("%s, ",$1);}' lfaa_requirements_20170330.rw_email
+# Table 9, column 2 in Braun (2019) : braun_2019.txt
+# awk '{printf("%d, ",$1);}' braun_2019.txt
+# awk '{printf("%d, ",$2);}' braun_2019.txt
+def skalow_braun2019( freq_mhz, interpolation_kind='cubic' ) :
+   freq_mhz_array = [ 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350 ] # awk '{printf("%d, ",$1);}' braun_2019.txt
+   station_aot    = [ 0.102, 0.255, 0.420, 0.580, 0.781, 0.862, 0.885, 0.924, 0.965, 1.052, 1.119, 1.159, 1.171, 1.190, 1.195, 1.223, 1.235, 1.263, 1.226, 1.234, 1.282, 1.304, 1.297, 1.306, 1.314, 1.289, 1.294, 1.327, 1.286, 1.204, 1.156 ] # awk '{printf("%.3f, ",$2);}' braun_2019.txt
    
    aot_func = None
    
@@ -30,12 +31,12 @@ def lfaa_ska( freq_mhz, interpolation_kind='cubic' ) :
       print("ERROR : aot_func is None -> no A/T value returned")
       return None         
    
-   aot_ret = aot_func( freq_mhz )
+   aot_ret = aot_func( freq_mhz )*512 # 512 stations 
    return aot_ret
    
 
 def lfaa_sefd( freq_mhz, n_stations=512, interpolation_kind='cubic' ) :
-   aot = lfaa_ska( freq_mhz )
+   aot = skalow_braun2019( freq_mhz )
    aot_one = aot / n_stations
    
    return ((2.00*1380.00)/aot_one)
@@ -45,7 +46,7 @@ def aot2sefd( aot ) :
    
    
 def lfaa_per_station( freq_mhz , n_stations=512, interpolation_kind='cubic' ) :
-   aot_ska = lfaa_ska( freq_mhz, interpolation_kind=interpolation_kind )
+   aot_ska = skalow_braun2019( freq_mhz, interpolation_kind=interpolation_kind )
    
    return ( aot_ska / n_stations )
 
@@ -56,10 +57,10 @@ if __name__ == "__main__":
        freq_mhz = float( sys.argv[1] )
 
     interpolation_kind='cubic'
-    if len(sys.argv) >= 2 :
-       interpolation_kind = sys.argv[2]
+#    if len(sys.argv) >= 3 :
+#       interpolation_kind = sys.argv[2]
 
-    aot_ska = lfaa_ska( freq_mhz , interpolation_kind=interpolation_kind )
+    aot_ska = skalow_braun2019( freq_mhz , interpolation_kind=interpolation_kind )
     sefd_ska = aot2sefd( aot_ska )
     
     aot_ska_station = lfaa_per_station( freq_mhz , interpolation_kind=interpolation_kind )
