5974210507c8bcde70234c6bf484401f9196c987
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2022-03-08 18:57:50 +0800

    better handling wrong source name

--- astroquery_interface.py
+++ astroquery_interface.py
@@ -14,16 +14,29 @@ except :
 
 def get_radec_for_a_source( source_name ) :
    if has_astroquery :
-      source_info = Simbad.query_object( source_name )
-      # general :
-      # source_info = Simbad.query_object( source_name )
-      coord_str=source_info['RA'][0] + " " + source_info['DEC'][0]
-      coordinates = SkyCoord(coord_str, frame=FK5, unit=(u.hourangle, u.deg), obstime="J2000")
+      ra_deg = None
+      dec_deg = None
+      found = False
+   
+      try :
+         source_info = Simbad.query_object( source_name )
+         found = True
+         # general :
+         # source_info = Simbad.query_object( source_name )
+      except :
+         print("WARNING : could not find object %s in SIMBAD catalogue" % (source_name))
+         found = False
+      
+      if found and source_info is not None :
+         coord_str=source_info['RA'][0] + " " + source_info['DEC'][0]
+         coordinates = SkyCoord(coord_str, frame=FK5, unit=(u.hourangle, u.deg), obstime="J2000")
 
-      ra_deg=coordinates.ra.value
-      dec_deg=coordinates.dec.value
+         ra_deg=coordinates.ra.value
+         dec_deg=coordinates.dec.value
 
-      print("DEBUG : Right ascension = %.4f [deg] , declination = %.4f [deg]" % (ra_deg,dec_deg))
+         print("DEBUG : Right ascension = %.4f [deg] , declination = %.4f [deg]" % (ra_deg,dec_deg))
+      else : 
+         print("WARNING : source %s not found in the catalogue" % source_name)
       
       return (ra_deg,dec_deg)
    else :
