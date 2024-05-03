3b4146436ac54b4c72c59f11fb62a890380aec21
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-05-03 13:20:54 +0800

    added merging of tsky

---
+++ scripts/merge_simulated_tsky.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# cat ??????????_az0_za0_eda_sensitivity_XX.txt | grep -v "#"
+
+pol=XX
+if [[ -n "$1" && "$1" != "-" ]]; then
+   pol=$1
+fi
+
+for file in `ls ??????????_az0_za0_eda_sensitivity_${pol}.txt`
+do
+   gps=`echo $file | cut -b 1-10`
+   ux=`gps2ux! $gps`
+   aot=`cat $file | grep -v "#" | tail -1 | awk '{print $2;}'`
+   tsky=`cat $file | grep -v "#" | tail -1 | awk '{print $6;}'`
+
+   if [[ -n $aot ]]; then   
+      echo $ux" "$tsky
+   fi
+done
